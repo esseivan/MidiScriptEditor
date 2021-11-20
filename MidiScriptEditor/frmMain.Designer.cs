1d3e461bf67abc1fe7515960ba29f826eec0ce27
EsseivaN <nicolas.esseiva@gmail.com>
2021-11-20 13:47:45 +0100

    Added user customisation

---
+++ MidiScriptEditor/frmMain.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace MidiScriptEditor
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.ofd = new System.Windows.Forms.OpenFileDialog();
+			this.sfd = new System.Windows.Forms.SaveFileDialog();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Load file";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.btnLoadFile_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(93, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Execute script";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.btnExecute_Click);
+			// 
+			// ofd
+			// 
+			this.ofd.DefaultExt = "mid";
+			this.ofd.Filter = "Midi file (*.mid, *.midi)|*.mid,*.midi|All files (*.*)|*.*";
+			this.ofd.ReadOnlyChecked = true;
+			// 
+			// sfd
+			// 
+			this.sfd.DefaultExt = "mid";
+			// 
+			// listBox1
+			// 
+			this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(12, 67);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(156, 303);
+			this.listBox1.TabIndex = 2;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(174, 83);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(278, 21);
+			this.comboBox1.TabIndex = 3;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(174, 67);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 13);
+			this.label1.TabIndex = 4;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(12, 406);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Add";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(93, 406);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 6;
+			this.button4.Text = "Del";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(54, 380);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            127,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(75, 20);
+			this.numericUpDown1.TabIndex = 4;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(464, 441);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "frmMain";
+			this.Text = "Midi Script";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.OpenFileDialog ofd;
+		private System.Windows.Forms.SaveFileDialog sfd;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+	}
+}
+
--- MidiScriptEditor/frmMain.cs
+++ MidiScriptEditor/frmMain.cs
@@ -28,12 +28,17 @@ using System.Windows.Forms;
 
 namespace MidiScriptEditor
 {
-	public partial class Form1 : Form
+	public partial class frmMain : Form
 	{
 		private string path = string.Empty;
+		public Dictionary<byte, Notes> Config;
+		public Dictionary<byte, string> Comments;
+		private bool pauseCheckEdit = true;
 
 		public enum Notes
 		{
+			Undefined = 0,
+
 			Kick = 36,
 			BassDrum = 35,
 			Rimshot = 37,
@@ -57,69 +62,59 @@ namespace MidiScriptEditor
 			Cowbell = 56,
 		}
 
-		public static byte GetNote(Notes note)
+		public byte GetNote(byte note)
 		{
-			return (byte)note;
+			Notes newNote = Config[note];
+			if (Enum.IsDefined(typeof(Notes), newNote) && newNote != Notes.Undefined)
+				return (byte)newNote;
+			return note;
 		}
 
-		public Form1()
+		private void PopulateConfig()
+		{
+			Config = new Dictionary<byte, Notes>()
+			{
+				{96, Notes.Kick }, // Orange Long Note - Kick
+				{97, Notes.Snare }, // Red Pad Lane 1 - Normal Snare/Ghost Note Snare
+				{98, Notes.Crash }, // Yellow Cymbal Lane 2 - Crash/Splash/Bell
+				{99, Notes.China }, // Blue Cymbal Lane 4 - China/Ride/Bell/Crash/Splash
+				{100, Notes.Ride }, // Green Cymbal Lane 6 - Crash/China/Splash/Bell
+				{110, Notes.LowMidTom }, // Yellow Pad Lane 3 - Tom 1
+				{111, Notes.LowTom }, // Blue Pad Lane 5 - Tom 2
+				{112, Notes.LowFloorTom }, // Green Pad Lane 7 - Tom 3
+			};
+			Comments = new Dictionary<byte, string>()
+			{
+				{96, "Orange Long Note - Kick"},
+				{97, "Red Pad Lane 1 - Normal Snare/Ghost Note Snare"},
+				{98, "Yellow Cymbal Lane 2 - Crash/Splash/Bell"},
+				{99, "Blue Cymbal Lane 4 - China/Ride/Bell/Crash/Splash"},
+				{100, "Green Cymbal Lane 6 - Crash/China/Splash/Bell"},
+				{110, "Yellow Pad Lane 3 - Tom 1"},
+				{111, "Blue Pad Lane 5 - Tom 2" },
+				{112, "Green Pad Lane 7 - Tom 3"},
+			};
+			comboBox1.DataSource = Enum.GetNames(typeof(Notes));
+			listBox1.DataSource = Config.Select((kv) => kv.Key).ToList();
+		}
+
+		private void RefreshDataSource()
+		{
+			listBox1.DataSource = null;
+			listBox1.DataSource = Config.Select((kv) => kv.Key).ToList();
+		}
+
+		public frmMain()
 		{
 			InitializeComponent();
-
+			PopulateConfig();
 			ofd.InitialDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
 			ofd.Filter = sfd.Filter = "Midi files (*.mid;*.midi)|*.mid;*.midi|All files (*.*)|*.*";
 		}
 
 		private void EditNote(NoteVoiceMidiEvent ev)
 		{
-			byte newNote = ev.Note;
-			switch (ev.Note)
-			{
-				// Orange Long Note - Kick
-				case 96:
-					newNote = GetNote(Notes.Kick);
-					break;
-
-				// Red Pad Lane 1 - Normal Snare/Ghost Note Snare
-				case 97:
-					newNote = GetNote(Notes.Snare);
-					break;
-
-				// Yellow Cymbal Lane 2 - Crash/Splash/Bell
-				case 98:
-					newNote = GetNote(Notes.Crash);
-					break;
-
-				// Blue Cymbal Lane 4 - China/Ride/Bell/Crash/Splash
-				case 99:
-					newNote = GetNote(Notes.China);
-					break;
-
-				// Green Cymbal Lane 6 - Crash/China/Splash/Bell
-				case 100:
-					newNote = GetNote(Notes.Ride);
-					break;
-
-				// Yellow Pad Lane 3 - Tom 1
-				case 110:
-					newNote = GetNote(Notes.LowMidTom);
-					break;
-
-				// Blue Pad Lane 5 - Tom 2
-				case 111:
-					newNote = GetNote(Notes.LowTom);
-					break;
-
-				// Green Pad Lane 7 - Tom 3
-				case 112:
-					newNote = GetNote(Notes.LowFloorTom);
-					break;
-
-				default:
-					break;
-			}
-			if (ev.Note != newNote)
-				ev.Note = newNote;
+			ev.Note = GetNote(ev.Note);
 		}
 
 		private void Script(string savePath)
@@ -213,6 +208,55 @@ namespace MidiScriptEditor
 		private void btnExecute_Click(object sender, EventArgs e)
 		{
 			Execute();
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (listBox1.SelectedIndex == -1)
+			{
+				label1.Text = string.Empty;
+				comboBox1.SelectedIndex = -1;
+				return;
+			}
+
+			pauseCheckEdit = true;
+
+			byte selConfig = (byte)listBox1.SelectedItem;
+			Notes selConfigNote = Config[selConfig];
+			comboBox1.SelectedItem = selConfigNote.ToString();
+			label1.Text = Comments[selConfig];
+
+			pauseCheckEdit = false;
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (pauseCheckEdit)
+				return;
+
+			if (comboBox1.SelectedIndex == -1)
+				return;
+
+
+			byte selConfig = (byte)listBox1.SelectedItem;
+			if (!Enum.TryParse(comboBox1.SelectedItem.ToString(), out Notes selNote))
+				return;
+
+			Config[selConfig] = selNote;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			Config[(byte)numericUpDown1.Value] = Notes.Undefined;
+			Comments[(byte)numericUpDown1.Value] = "Custom value";
+			RefreshDataSource();
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			Config.Remove((byte)numericUpDown1.Value);
+			Comments.Remove((byte)numericUpDown1.Value);
+			RefreshDataSource();
 		}
 	}
 }
